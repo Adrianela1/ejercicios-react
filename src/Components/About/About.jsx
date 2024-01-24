b8188f1fbddeb12931d40303febd90f2e67097ff
Adrianela1 <nemesiososo@gmail.com>
2024-01-24 13:44:25 -0600

    agregue about con info

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,9 +1,43 @@
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+    <section>
+        <article>
+            <div>
+                <img src="https://avatars.githubusercontent.com/u/124970237?v=4" alt=""></img>
+            </div>
+        </article>
+        <aside>
+            <h1>Adrian Nevarez</h1>
+            <p>
+                <b>A passionate backend developer</b>
+            </p>
+            <ul>
+                <li>x</li>
+                <li>x</li>
+                <li>x</li>
+            </ul>
+            <p>
+                <b>Contact with me</b>
+            </p>
+            <p>Linkedin</p>
+            <p>Github</p>
+            <p>Gmail</p>
+
+            <section>
+                <p>Languages and Tools</p>
+                <ul>
+                    <li>Java</li>
+                    <li>Spring boot</li>
+                    <li>JavaScript</li>
+                    <li>Postman</li>
+                    <li>Insomnia</li>
+                    <li>Html</li>
+                    <li>CSS</li>
+                </ul>
+            </section>
+        </aside>
+    </section>
   )
 }
 
